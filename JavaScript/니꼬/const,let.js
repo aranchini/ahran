271565aaabd8312c4441ebc94d8b47c7680d1baa
aranchini <s26032@gsm.hs.kr>
2026-07-06 23:16:15 +0900

    JS

---
+++ JavaScript/니꼬/const,let.js
@@ -0,0 +1,12 @@
+let a = 5;
+const b = 2;
+
+const myName = "nico";
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+console.log("hello " + "nico");
+
+// const는 상수,값이 바뀔수 없음
+// let은 변수,값이 바뀔수 있음
