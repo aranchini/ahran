c73eb95c049256a794c958ba1b6eb247fe046005
aranchini <s26032@gsm.hs.kr>
2026-07-07 21:07:57 +0900

    nico

---
+++ JavaScript/니꼬/Arrays.js
@@ -0,0 +1,13 @@
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+//array에서 아이템 받아오기
+console.log(daysOfWeek[4]);
+console.log(daysOfWeek);
+
+// array 안에 요일 더 추가하기
+daysOfWeek.push("sun")
+console.log(daysOfWeek);
+
+//설명이 필요하지 않은 데이터 리스트들은 array로,
+//설명이 필요한 정보가 담긴 데이터 리스트들은 object로!
