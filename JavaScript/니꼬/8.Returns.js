1b0cf3fce0f006de593d6f45812f8ac71bbaf832
aranchini <s26032@gsm.hs.kr>
2026-07-08 12:20:40 +0900

    nico

--- JavaScript/니꼬/8.Returns.js
+++ JavaScript/니꼬/8.Returns.js
@@ -30,3 +30,8 @@ const minusResult = calculator.minus(plusResult,10);
 const multiResult = calculator.multi(10,minusResult);
 const divideResult = calculator.divide(multiResult,plusResult);
 const powerResult = calculator.power(divideResult,minusResult);
+
+//console.log는 그림의 떡. 꺼내 먹으려면 return필요....
+//return하면 함수 끝남
+//함수는 실행하면 사라지고 마지막엔 결과를 남김
+//마지막에 return이 없으면 undefined를 출력
