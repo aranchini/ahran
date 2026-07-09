ad06053d5c1f14e0f4045046c0d199a828943711
aranchini <s26032@gsm.hs.kr>
2026-07-09 16:46:23 +0900

    nico

--- nico/23.Loading user.js
+++ nico/23.Loading user.js
@@ -28,3 +28,26 @@ if (savedUsername === null) {
 } else {
     paintGreetings(savedUsername);
 }
+
+/*<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Momentum</title>
+  </head>
+  <body>
+    <form class="hidden" id="login-form">
+      <input
+        required
+        maxlength="15"
+        type="text"
+        placeholder="이름이 뭐에요?"
+      />
+      <input type="submit" value="Log In" />
+    </form>
+    <h1 class="hidden" id="greeting"></h1>
+    <script src="23.Loading user.js"></script>
+  </body>
+</html>*/
