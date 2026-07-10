9fdcdc21e8a91d791528735286725c45980c8f3c
aranchini <s26032@gsm.hs.kr>
2026-07-10 10:20:15 +0900

    nico

--- nico/31.Deleting todo.js
+++ nico/31.Deleting todo.js
@@ -1,4 +1,34 @@
-<!DOCTYPE html>
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText= newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click",deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo =toDoInput.value;
+    toDoInput.value="";
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
+
+/*<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -30,6 +60,6 @@
     <script src = "greetings.js"></script>
     <script src="27.Quotes.js"></script>
     <script src="28.background.js"></script>
-    <script src="32.Saving todo.js"></script>
+    <script src="31.Deleting todo.js"></script>
   </body>
-</html>
+</html> */
